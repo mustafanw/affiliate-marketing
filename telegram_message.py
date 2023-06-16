f3bb88fc1ffef4a764936ff47317d0413acb3d09
mustafanw <mustafanw.code@gmail.com>
2023-06-16 21:52:19 +0530

    added two channels

--- telegram_message.py
+++ telegram_message.py
@@ -5,11 +5,13 @@ from affiliates import TeleGramAffiliate
 
 
 class TelegramMessageForwarder:
-    def __init__(self, api_id, api_hash, source_channel, destination_channel):
+    def __init__(self, api_id, api_hash, source_channel, dest_chn1, dest_chn2, test_chn):
         self.api_id = api_id
         self.api_hash = api_hash
         self.source_channel = source_channel
-        self.destination_channel = destination_channel
+        self.dest_chn1 = dest_chn1
+        self.dest_chn2 = dest_chn2
+        self.test_chn = test_chn
         self.client = TelegramClient('session', api_id, api_hash)
         self.telegram_affiliate = TeleGramAffiliate()
 
@@ -20,7 +22,8 @@ class TelegramMessageForwarder:
         event.message.message = message
 
         try:
-            await self.client.send_message(self.destination_channel, message)
+            await self.client.send_message(self.dest_chn1, message)
+            await self.client.send_message(self.dest_chn2, message)
             print(f"Message forwarded: {message}")
         except Exception as e:
             print(f"Failed to forward message: {str(e)}")
@@ -28,14 +31,17 @@ class TelegramMessageForwarder:
     def start(self):
         print("Start")
         self.client.add_event_handler(self.forward_messages, events.NewMessage(chats=self.source_channel))
+        self.client.add_event_handler(self.forward_messages, events.NewMessage(chats=self.test_chn))
         self.client.start()
         self.client.run_until_disconnected()
 
 # Usage example
 api_id = 1958908
 api_hash = '21d35d34c158be29142b85527b4128ab'
-source_channel = 'testgmnw'
-destination_channel = 'testgmnw'
+source_channel = 'stealsales'
+test_chn = 'testgmnw'
+dest_chn1 = 'musaira_deals'
+dest_chn2 = 'musairadeals'
 
-forwarder = TelegramMessageForwarder(api_id, api_hash, source_channel, destination_channel)
+forwarder = TelegramMessageForwarder(api_id, api_hash, source_channel, dest_chn1, dest_chn2, test_chn)
 forwarder.start()
